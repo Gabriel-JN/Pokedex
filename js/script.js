9353b003377a0c2348560ec4d0d4f8a288728a8a
Gabriel-JN <gabriel.jorge19999@gmail.com>
2022-12-04 00:20:15 -0300

    Getting the Pokemon image from the data API

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
 const fetchPokemon = async (pokemon) => {
 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-
     const data = await APIResponse.json();
-
-    console.log(data);
+    return data;
 
 }
 
-fetchPokemon('25');
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+}
+
+renderPokemon('25');
